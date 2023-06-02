f7cdc4fdbb12c34c540374fbb1f366b988497d30
Nilesh Dalagade <79205797+nsd8888@users.noreply.github.com>
2023-06-02 14:57:44 +0530

    Update settings.py

--- City_Population/settings.py
+++ City_Population/settings.py
@@ -76,16 +76,16 @@ WSGI_APPLICATION = 'City_Population.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-        # 'NAME': "new_project",
-        # 'USER': 'root',  
-        # 'PASSWORD': 'Blucognition@88',  
-        # 'HOST': '127.0.0.1',  
-        # 'PORT': '3306',  
-        # 'OPTIONS': {  
-        #     'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
-        # }
+        'ENGINE': 'django.db.backends.mysql',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': "citydb",
+        'USER': 'admin',  
+        'PASSWORD': 'Django123',  
+        'HOST': 'databasedj.cetjcletfp74.us-east-1.rds.amazonaws.com',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+         }
         
     }
 }
